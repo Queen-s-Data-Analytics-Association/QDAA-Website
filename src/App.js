24dafbc70ab9902cc0dbf9329658058d69c5bd64
Joshua Wu <wujoshua56@gmail.com>
2024-02-04 22:41:34 -0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import Navbar from "./Navbar";
+import Home from "./pages/Home";
+import Team from "./pages/Team";
+import Projects from "./pages/Projects";
+import Events from "./pages/Events";
+import Sponsors from "./pages/Sponsors";
+
 
 function App() {
-  return <Navbar />
+  let component
+  switch (window.location.pathname) {
+    case "/":
+      component = <Home />
+      break
+    case "/team":
+      component = <Team />
+      break
+    case "/projects":
+      component = <Projects />
+      break
+    case "/events":
+      component = <Events />
+      break
+    case "/sponsors":
+      component = <Sponsors />
+      break
+  }
+  return (
+  <>
+    <Navbar />
+    {component}
+  </>
+  )
 }
 
 export default App;
