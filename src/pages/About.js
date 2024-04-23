1c199420081584eeaa58f18d606a23dca3cb26d5
Joshua Wu <wujoshua56@gmail.com>
2024-04-23 18:59:43 -0400

    start about page implementation

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,17 @@
 export default function About() {
+    let message = "Providing Queen’s University students with the opportunity to learn about data and develop analytical skills through the following:"
     return (
-      <>
-    
-      </>
+      <section className="section-white">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12 text-center">
+                        <h2 className="section-title">
+                            Queen's Data Analytics Association
+                        </h2>
+                        <p className="section-subtitle">{message}</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
